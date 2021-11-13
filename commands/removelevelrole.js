4ab32c76a9b1b1ee797b1f60e23d70b30d3262f0
Tominous <18189490+Tominous@users.noreply.github.com>
2021-11-13 15:49:13 +0000

    Update removelevelrole.js

--- commands/removelevelrole.js
+++ commands/removelevelrole.js
@@ -11,8 +11,8 @@ exports.run = (client, msg, args) => {
 
    
     let role = msg.guild.roles.cache.find(r => r.name === args.slice(0, args.length-1).join(" ")) || msg.guild.roles.cache.get(args.slice(0, args.length-1).join(" ")) || msg.mentions.roles.first()
-   // if (msg.guild.roles.get(args[0])) role = msg.guild.roles.get(args[0])
-   // if (msg.guild.roles.find(r => r.name === args[0])) role = msg.guild.roles.find(r => r.name === args[0])
+    if (msg.guild.roles.get(args[0])) role = msg.guild.roles.get(args[0])
+    if (msg.guild.roles.find(r => r.name === args[0])) role = msg.guild.roles.find(r => r.name === args[0])
 
 
     if (!role) return msg.channel.send('I could not find that role.')
